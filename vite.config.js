1879b60d8d0b376d2e2c5d7f1186fa3496807999
Deepak Bawa <24bcs10812@cuchd.in>
2025-12-07 22:04:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-day-2/',   // MUST match your repo name exactly
+  base: '/react-day-2/',
   plugins: [react()],
 })
